bc90bb53ef0e3e88b40c6c6996af84417f5dbbd7
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-02-15 00:19:53 +0900

    create model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'ideas/index'
+  get 'ideas/show'
+  get 'ideas/new'
+  get 'ideas/edit'
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
