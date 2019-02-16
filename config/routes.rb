95d8a14736f015b188097a5c3e9f8018f5f751cd
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-02-17 00:59:12 +0900

    add action into index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'ideas/index'
-  get 'ideas/show'
-  get 'ideas/new'
-  get 'ideas/edit'
+    #get 'ideas/index'
+    #get 'ideas/show'
+    #get 'ideas/new'
+    #get 'ideas/edit'
+  root 'ideas#index'  
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
