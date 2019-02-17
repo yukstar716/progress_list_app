0c2f90510debd69707c83bb265341d8842b200ea
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-02-17 17:27:12 +0900

    add new and controller action

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,8 +7,21 @@ class IdeasController < ApplicationController
   end
 
   def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.save
+    redirect_to ideas_path
   end
 
   def edit
   end
+
+  private
+    def idea_params
+      params.require(:idea).permit(:title, :content)
+    end
+
 end
