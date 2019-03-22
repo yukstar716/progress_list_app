e465890785a727b51d3912e41cb0d4a35104901e
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2019-03-22 19:55:08 +0900

    add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_052814) do
+ActiveRecord::Schema.define(version: 2019_02_26_135829) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", default: 0, null: false
-    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "remember_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
